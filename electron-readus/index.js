55c1a5b91dae001e9aaef4db80a30d22eee1695e
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2015-09-22 16:10:13 +0900

    Add README.md listing feature with fileUtil.

--- electron-readus/index.js
+++ electron-readus/index.js
@@ -1,24 +1,9 @@
 'use strict';
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+//var remote = require('remote');
+//var fileUtil = remote.require('./lib/fileUtil');
+var fileUtil = require('./lib/fileUtil');
 
-require('crash-reporter').start();
-
-var mainWindow = null;
-
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
-    app.quit();
-});
-
-app.on('ready', function() {
-
-  // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+fileUtil.fetchReadmeList(function(err, matches) {
+  if(!err) document.write(matches.join());
 });
