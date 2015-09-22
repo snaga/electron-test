30429eaff78d85d071c4cb881ee7ed666738d518
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2015-09-22 18:01:22 +0900

    Add Postgres connectivity with node-postgres.

--- electron-readus/index.js
+++ electron-readus/index.js
@@ -1,9 +1,27 @@
 'use strict';
 
-//var remote = require('remote');
-//var fileUtil = remote.require('./lib/fileUtil');
-var fileUtil = require('./lib/fileUtil');
+var pg = require('pg');
 
-fileUtil.fetchReadmeList(function(err, matches) {
-  if(!err) document.write(matches.join());
+var connString = "tcp://postgres:postgres@localhost:5432/postgres";
+
+var html = ""
+
+html = html + "io.js: " + process.version + "<br/>"
+html = html + "Electron: " + process.versions['electron'] + "<br/>"
+html = html + "Chrome: " + process.versions['chrome'] + "<br/>"
+
+pg.connect(connString, function(err, conn) {
+  html = html + "<h1>Hello, PostgreSQL</h1>";
+
+  html = html + "connString: " + connString + "<br/><br/>";
+
+  conn.query("SELECT version();", function(err, rs) {
+    console.log("rs length:" + rs.rows.length);
+
+    var i;
+    for(i = 0 ; i < rs.rows.length ; i++) {
+      html = html + "version: " + rs.rows[i].version + "<br/>";
+    }
+    document.getElementById("body").innerHTML = html;
+  });
 });
